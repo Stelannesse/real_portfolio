9f237dcff157822e6c68811db1e6ff18b6931175
Stelannesse <dreakian@gmail.com>
2025-03-18 18:11:38 +0100

    ajout images

---
+++ Travaux_et_autres/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace jeu_bataille
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::jeu_bataille.Properties.Resources.trefle_14;
+            this.pictureBox2.Location = new System.Drawing.Point(273, 118);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(260, 350);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::jeu_bataille.Properties.Resources.trefle_14;
+            this.pictureBox1.Location = new System.Drawing.Point(871, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(879, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 57);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Mélanger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Piocher";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(567, 502);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(252, 58);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Comparer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nom des joueurs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Résultat des tours";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(47, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 344);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(1166, 130);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(184, 344);
+            this.listBox2.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(567, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(266, 50);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Commencer la partie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(978, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(567, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(266, 50);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Jouer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(664, 432);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 704);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Travaux_et_autres/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jeu_bataille
+{
+    public partial class Form1 : Form
+    {
+        private Paquet pack;
+        private Joueur J1;
+        private Joueur J2;
+        Random random =new Random();
+        List<Carte> packJ1, packJ2, packBataille;
+        int i;
+        int nbr_cartes = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+
+            pack = new Paquet();
+
+            packJ1 = new List<Carte>();
+            packJ2 = new List<Carte>();
+
+            pack.MelangerCarte();
+            
+            for (int i = 0; i < 16; i++)
+            {
+                packJ1.Add(pack.GetCartes());
+                listBox1.Items.Add(packJ1[i].getCouleur() + " " +packJ1[i].getValeur());
+                packJ2.Add(pack.GetCartes());
+                listBox2.Items.Add(packJ2[i].getCouleur()+ " " + packJ2[i].getValeur());
+
+            }
+
+            label3.Text = packJ1.Count().ToString();
+            label4.Text = packJ2.Count().ToString();
+          
+            
+            J1 = new Joueur("azertyui", packJ1);
+            J2 = new Joueur("sdfui", packJ2);
+
+            label1.Text = J1.GetNom();
+            label2.Text = J2.GetNom();
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            packBataille = new List<Carte>();
+            pictureBox2.ImageLocation = ".\\Cartes\\" + packJ1[0].getCouleur() + packJ2[0].getValeur() + ".jpg";
+            pictureBox1.ImageLocation = ".\\Cartes\\" + packJ2[0].getCouleur() + packJ2[0].getValeur() + ".jpg";
+
+            
+            if (packJ1[i].getValeur() > packJ2[i].getValeur())
+            {
+                if (packBataille.Count > 0)
+                {
+                    foreach (Carte c in packBataille)
+                    {
+                        packJ1.Add(c);
+                    }
+                }
+                packJ1.Add(packJ2[i]);
+                listBox1.Items.Add(packJ2[i].getCouleur() + " " + packJ2[i].getValeur());
+                packJ2.RemoveAt(i);
+                listBox2.Items.RemoveAt(i);
+
+                label3.Text = packJ1.Count().ToString();
+                label4.Text = packJ2.Count().ToString();
+            }
+            else if (packJ2[i].getValeur() > packJ1[i].getValeur())
+            {
+                if (packBataille.Count > 0)
+                {
+                    foreach (Carte c in packBataille)
+                    {
+                        packJ2.Add(c);
+                    }
+                }
+                packJ2.Add(packJ1[i]);
+                listBox2.Items.Add(packJ1[i].getCouleur() + " " + packJ1[i].getValeur());
+                packJ1.RemoveAt(i);
+                listBox1.Items.RemoveAt(i);
+
+                label3.Text = packJ1.Count().ToString();
+                label4.Text = packJ2.Count().ToString();
+            }
+            else
+            {
+
+                packBataille.Add(packJ1[i]);
+                packBataille.Add(packJ2[i]);
+                i++;
+            }
+
+            if (packJ1.Count == 0)
+            {
+                label5.Visible = true;
+                label5.Text = "Le joureur 1 a gagné la partie";
+            }
+            if (packJ2.Count == 0)
+            {
+                label5.Visible = true;
+                label5.Text = "Le joureur 2 a gagné la partie";
+            }
+            /**/
+        }
+    }
+}
